5fdea8df5e310fecb74a454684c957807d45d1a2
adrian <nyancatgaming9999@gmail.com>
2025-09-15 18:42:24 -0700

    manifest

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const Database = require("better-sqlite3");
 const express = require("express");
 
 const db = new Database("data/warehouse.db");
+db.exec("");
 
 const app = express();
+
+app.use(express.static("static"));
 app.listen(3000, "127.0.0.1", () => console.log("server now listening!"));
